78b9bf7470678cd44791194ff8f96f0f9e21086d
hctel <hugo.de.hepcee@gmail.com>
2024-11-28 09:12:43 +0100

    Added Animal.cs

---
+++ GameOfLife/ViewModels/Animal.cs
@@ -0,0 +1,19 @@
+using Avalonia;
+using CommunityToolkit.Mvvm.ComponentModel
+
+namespace gol.ViewModels;
+
+public class Animal : LifeForm
+{
+	[ObservableProperty]
+	private int visionZone;
+
+	[ObservableProperty]
+	private int contactZone;
+
+	public Animal(Point location, int health, int visionZone, int contactZone) : base(location, health)
+	{
+		this.contactZone = contactZone;
+		this.visionZone = visionZone;
+	}
+}
