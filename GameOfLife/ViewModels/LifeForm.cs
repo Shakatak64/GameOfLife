9f1b57da12b37eed267772fe25b05dc43312f4bb
Deuvaert Victor <22054@ecam.be>
2024-11-28 09:05:34 +0100

    Add LifeForm class

---
+++ GameOfLife/ViewModels/LifeForm.cs
@@ -0,0 +1,19 @@
+using Avalonia;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace gol.ViewModels;
+
+public partial class LifeForm : GameObject {
+    [ObservableProperty]
+    private int health;
+
+    private int counter = 0;
+
+    public LifeForm(Point location, int health) : base(location) {
+        this.health = health;
+    }
+
+    public void Tick() {
+        counter++;    
+    }
+}
