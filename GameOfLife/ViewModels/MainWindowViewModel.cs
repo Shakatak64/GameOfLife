e180cd9c270f3fbd4095ce57ad997b55b3c60fc4
hctel <hugo.de.hepcee@gmail.com>
2024-12-09 10:54:43 +0100

    Fixed merge error

--- GameOfLife/ViewModels/MainWindowViewModel.cs
+++ GameOfLife/ViewModels/MainWindowViewModel.cs
@@ -17,6 +17,7 @@ public partial class MainWindowViewModel : GameBase
 
     public MainWindowViewModel()
     {
+        r = new Random();
         GameObjects.Add(new Plant(new Point(120, 600), 100, 100, 100, 100));
         GameObjects.Add(new Herbivore(new Point(700, 600), 100, 100, 500, 100, Gender.MALE, 100, @"..\..\..\Assets\sheep.wav"));
         GameObjects.Add(new Carnivore(new Point(200, 0), 100, 100, 1000, 100, Gender.MALE, 100, @"..\..\..\Assets\sheep.wav"));
