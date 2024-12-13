cfd4c17dcca78aedc39899d0e4f16088fcf97d44
Deuvaert Victor <22054@ecam.be>
2024-12-13 14:30:19 +0100

    Make default random spawn

--- GameOfLife/ViewModels/MainWindowViewModel.cs
+++ GameOfLife/ViewModels/MainWindowViewModel.cs
@@ -19,9 +19,26 @@ public partial class MainWindowViewModel : GameBase
     public MainWindowViewModel()
     {
         r = new Random();
-        GameObjects.Add(new Plant(new Point(120, 600), 100, 100, 100, 100));
-        GameObjects.Add(new Herbivore(new Point(700, 600), 50, 100, 500, 100, Gender.MALE, 100, @"..\..\..\Assets\sheep.wav"));
-        GameObjects.Add(new Carnivore(new Point(200, 0), 100, 100, 1000, 100, Gender.MALE, 100, @"..\..\..\Assets\wolf.wav"));
+
+        for (int i = 0; i < 10; i++)
+        {
+            if (i % 2 == 0)
+            {
+                GameObjects.Add(new Plant(new Point(r.NextInt64(1000), r.NextInt64(1000)), 100, 100, 100, 100));
+            }
+            else if (i % 3 == 0)
+            {
+                GameObjects.Add(new Herbivore(new Point(r.NextInt64(1000), r.NextInt64(1000)), 50, 100, 500, 100, Gender.MALE, 100, @"..\..\..\Assets\sheep.wav"));
+            }
+            else if (i % 5 == 0)
+            {
+                GameObjects.Add(new Carnivore(new Point(r.NextInt64(1000), r.NextInt64(1000)), 100, 100, 1000, 100, Gender.MALE, 100, @"..\..\..\Assets\wolf.wav"));
+            }
+        }
+
+        //GameObjects.Add(new Plant(new Point(120, 600), 100, 100, 100, 100));
+        //GameObjects.Add(new Herbivore(new Point(700, 600), 50, 100, 500, 100, Gender.MALE, 100, @"..\..\..\Assets\sheep.wav"));
+        //GameObjects.Add(new Carnivore(new Point(200, 0), 100, 100, 1000, 100, Gender.MALE, 100, @"..\..\..\Assets\wolf.wav"));
     }
 
     protected override void Tick()
